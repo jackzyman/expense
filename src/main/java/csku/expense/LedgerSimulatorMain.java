b07fd50d6b01dba762f496d38c42c106e38df9f6
jackzyman <jarukit.r@ku.th>
2018-09-15 19:28:39 +0700

    edit logs file

--- src/main/java/csku/expense/LedgerSimulatorMain.java
+++ src/main/java/csku/expense/LedgerSimulatorMain.java
@@ -1,9 +1,7 @@
 package csku.expense;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+
 
 public class LedgerSimulatorMain {
     public static void main(String[] args) throws IOException {
